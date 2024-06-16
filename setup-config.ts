f48d152577c6600980cb1b2e16c16d87163d5b70
Rosendo <grosendoh73@gmail.com>
2024-06-15 23:55:15 -0600

    🐛 Fix more bugs

--- setup-config.ts
+++ setup-config.ts
@@ -5,65 +5,61 @@ import path from 'path'
 import inquirer from 'inquirer'
 import { execSync } from 'child_process'
 
-const eslintConfig = `
-  /** @type {import('eslint').Linter.Config} */
-  module.exports = {
-    extends: ['plugin:astro/recommended', 'ts-standard'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      tsconfigRootDir: __dirname,
-      sourceType: 'module',
-      ecmaVersion: 'latest'
-    },
-    overrides: [
-      {
-        files: ['*.astro'],
-        parser: 'astro-eslint-parser',
-        parserOptions: {
-          parser: '@typescript-eslint/parser',
-          extraFileExtensions: ['.astro']
-        },
-        rules: {
-          // override/add rules settings here, such as:
-          'astro/no-set-html-directive': 'error'
-        }
+const eslintConfig = `/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ['plugin:astro/recommended', 'ts-standard'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaVersion: 'latest'
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        'astro/no-set-html-directive': 'error'
       }
-    ]
-  }
+    }
+  ]
+}
 `
 
 const prettierIgnore = 'node_modules/**'
 
-const prettierConfig = `
-  {
-    'useTabs': false,
-    'singleQuote': true,
-    'trailingComma': 'none',
-    'semi': false,
-    'printWidth': 100,
-    'plugins': ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
-    'jsxSingleQuote': true,
-    'tabWidth': 2,
-    'endOfLine': 'lf'
-  }
-`
+const prettierConfig = `{
+  "useTabs": false,
+  "singleQuote": true,
+  "trailingComma": "none",
+  "semi": false,
+  "printWidth": 100,
+  "plugins": ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  "jsxSingleQuote": true,
+  "tabWidth": 2,
+  "endOfLine": "lf"
+}`
 
-const prettierConfigCJS = `
-  /** @type {import('prettier').Config} */
-  module.exports = {
-    // i am just using the standard config, change if you need something else
-    ...require('prettier-config-standard'),
-    pluginSearchDirs: [__dirname],
-    plugins: [require.resolve('prettier-plugin-astro')],
-    overrides: [
-      {
-        files: '*.astro',
-        options: {
-          parser: 'astro'
-        }
+const prettierConfigCJS = `/** @type {import('prettier').Config} */
+module.exports = {
+  // i am just using the standard config, change if you need something else
+  ...require('prettier-config-standard'),
+  pluginSearchDirs: [__dirname],
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
       }
-    ]
-  }
+    }
+  ]
+}
 `
 
 const dependencies = [
@@ -90,7 +86,7 @@ const main = async () => {
       type: 'list',
       name: 'packageManager',
       message: 'Which package manager do you use?',
-      choices: ['npm', 'yarn', 'pnpm'],
+      choices: ['pnpm', 'yarn', 'npm'],
       when: (answers) => answers.installDeps
     }
   ])
@@ -106,19 +102,23 @@ const main = async () => {
 
     execSync(installCommand, { stdio: 'inherit' })
     console.log('Dependencies installed!')
+  } else {
+    console.log('Skipping dependency installation...')
   }
 
-  fs.writeFileSync(path.join(process.cwd(), 'eslintrc.cjs'), eslintConfig)
-  fs.writeFileSync(path.join(process.cwd(), '.prettierignore'), prettierIgnore)
-  fs.writeFileSync(path.join(process.cwd(), '.prettierrc'), prettierConfig)
-  fs.writeFileSync(path.join(process.cwd(), 'prettier.config.cjs'), prettierConfigCJS)
+  if (answers.installDeps) {
+    fs.writeFileSync(path.join(process.cwd(), 'eslintrc.cjs'), eslintConfig)
+    fs.writeFileSync(path.join(process.cwd(), '.prettierignore'), prettierIgnore)
+    fs.writeFileSync(path.join(process.cwd(), '.prettierrc'), prettierConfig)
+    fs.writeFileSync(path.join(process.cwd(), 'prettier.config.cjs'), prettierConfigCJS)
 
-  console.log('Setup complete! 🚀')
+    console.log('Setup complete! 🚀 Happy coding ✨')
 
-  console.log(`
+    console.log(`
     Add the following script to your package.json file:
     "lint": "prettier --write  \\"**/*.{js,jsx,ts,tsx,md,mdx,astro}\\" && eslint --fix \\"src/**/*.{js,ts,jsx,tsx,astro}\\""
-  `)
+    `)
+  }
 }
 
 main()
--- src/setup-config.js
+++ src/setup-config.js
@@ -3,62 +3,58 @@ import fs from 'fs';
 import path from 'path';
 import inquirer from 'inquirer';
 import { execSync } from 'child_process';
-const eslintConfig = `
-  /** @type {import('eslint').Linter.Config} */
-  module.exports = {
-    extends: ['plugin:astro/recommended', 'ts-standard'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      tsconfigRootDir: __dirname,
-      sourceType: 'module',
-      ecmaVersion: 'latest'
-    },
-    overrides: [
-      {
-        files: ['*.astro'],
-        parser: 'astro-eslint-parser',
-        parserOptions: {
-          parser: '@typescript-eslint/parser',
-          extraFileExtensions: ['.astro']
-        },
-        rules: {
-          // override/add rules settings here, such as:
-          'astro/no-set-html-directive': 'error'
-        }
+const eslintConfig = `/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ['plugin:astro/recommended', 'ts-standard'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaVersion: 'latest'
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        'astro/no-set-html-directive': 'error'
       }
-    ]
-  }
+    }
+  ]
+}
 `;
 const prettierIgnore = 'node_modules/**';
-const prettierConfig = `
-  {
-    'useTabs': false,
-    'singleQuote': true,
-    'trailingComma': 'none',
-    'semi': false,
-    'printWidth': 100,
-    'plugins': ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
-    'jsxSingleQuote': true,
-    'tabWidth': 2,
-    'endOfLine': 'lf'
-  }
-`;
-const prettierConfigCJS = `
-  /** @type {import('prettier').Config} */
-  module.exports = {
-    // i am just using the standard config, change if you need something else
-    ...require('prettier-config-standard'),
-    pluginSearchDirs: [__dirname],
-    plugins: [require.resolve('prettier-plugin-astro')],
-    overrides: [
-      {
-        files: '*.astro',
-        options: {
-          parser: 'astro'
-        }
+const prettierConfig = `{
+  "useTabs": false,
+  "singleQuote": true,
+  "trailingComma": "none",
+  "semi": false,
+  "printWidth": 100,
+  "plugins": ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  "jsxSingleQuote": true,
+  "tabWidth": 2,
+  "endOfLine": "lf"
+}`;
+const prettierConfigCJS = `/** @type {import('prettier').Config} */
+module.exports = {
+  // i am just using the standard config, change if you need something else
+  ...require('prettier-config-standard'),
+  pluginSearchDirs: [__dirname],
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
       }
-    ]
-  }
+    }
+  ]
+}
 `;
 const dependencies = [
     '@typescript-eslint/parser@5.62.0',
@@ -83,7 +79,7 @@ const main = async () => {
             type: 'list',
             name: 'packageManager',
             message: 'Which package manager do you use?',
-            choices: ['npm', 'yarn', 'pnpm'],
+            choices: ['pnpm', 'yarn', 'npm'],
             when: (answers) => answers.installDeps
         }
     ]);
@@ -98,14 +94,19 @@ const main = async () => {
         execSync(installCommand, { stdio: 'inherit' });
         console.log('Dependencies installed!');
     }
-    fs.writeFileSync(path.join(process.cwd(), 'eslintrc.cjs'), eslintConfig);
-    fs.writeFileSync(path.join(process.cwd(), '.prettierignore'), prettierIgnore);
-    fs.writeFileSync(path.join(process.cwd(), '.prettierrc'), prettierConfig);
-    fs.writeFileSync(path.join(process.cwd(), 'prettier.config.cjs'), prettierConfigCJS);
-    console.log('Setup complete! 🚀');
-    console.log(`
+    else {
+        console.log('Skipping dependency installation...');
+    }
+    if (answers.installDeps) {
+        fs.writeFileSync(path.join(process.cwd(), 'eslintrc.cjs'), eslintConfig);
+        fs.writeFileSync(path.join(process.cwd(), '.prettierignore'), prettierIgnore);
+        fs.writeFileSync(path.join(process.cwd(), '.prettierrc'), prettierConfig);
+        fs.writeFileSync(path.join(process.cwd(), 'prettier.config.cjs'), prettierConfigCJS);
+        console.log('Setup complete! 🚀 Happy coding ✨');
+        console.log(`
     Add the following script to your package.json file:
     "lint": "prettier --write  \\"**/*.{js,jsx,ts,tsx,md,mdx,astro}\\" && eslint --fix \\"src/**/*.{js,ts,jsx,tsx,astro}\\""
-  `);
+    `);
+    }
 };
 main();
